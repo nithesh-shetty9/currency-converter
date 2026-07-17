8f3892ca68ef2de9da28f8cf042561a66757b2cd
Nithesh <nitheshnithesh310@gmail.com>
2026-07-17 07:34:11 +0530

    implement currency conversion logic and update input handling in the converter

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function convertCurrency() {
-    
+let base=document.getElementById("from").value;
+let  to=document.getElementById('to').value;
+let basevalue=document.getElementById('baseamount').value;
+let convertedvalue=document.getElementById('converted-amount');
+if(base===to)
+{
+    alert("please select different currency");
+}
+else if(!(basevalue===""))
+{
+       const API_KEY = "cur_live_eRMf4C91asHwCpfb2QlX1pW5xiuHHhn21FUIY0bM";
+
+fetch(`https://api.currencyapi.com/v3/latest?apikey=${API_KEY}&base_currency=${base}&currencies=${to}`)
+  .then((res) => res.json())
+  .then((data) => {
+let con=parseFloat(data.data[to].value);
+let b=parseFloat(basevalue);
+convertedvalue.value = (con * b).toFixed(2);
+let rate=document.querySelector('.rate');
+rate.innerHTML = `1 ${base} = ${con.toFixed(4)} ${to}`;
+}); 
+
+}
 }
